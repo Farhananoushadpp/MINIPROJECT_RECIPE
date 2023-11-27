902ffcc9f03f12d5f202875745d72da2fec6b120
mhdshibin <muhammedshibin96@gmail.com>
2023-11-27 21:34:53 +0530

    minor fix

--- lib/utils/search_functions.dart
+++ lib/utils/search_functions.dart
@@ -1,12 +1,16 @@
 import 'package:recipe_app/utils/string_functions.dart';
 
-bool isMatchFound(List<String> searchParameters, String ingredients) {
-  searchParameters = toLowerCaseConverter(searchParameters);
+bool isMatchFound(List<String> searchParameters, String? ingredients) {
+  if (ingredients == null) {
+    return false;
+  } else {
+    searchParameters = toLowerCaseConverter(searchParameters);
   ingredients = toLowerCaseConverter(ingredients);
   for (String parameter in searchParameters) {
-    if (!ingredients.contains(parameter)) {
+    if (!ingredients!.contains(parameter)) {
       return false;
     }
   }
   return true;
+  }
 }
